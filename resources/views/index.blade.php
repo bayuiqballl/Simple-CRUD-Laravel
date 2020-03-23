21294a32f3937e15b01c6b665c811f8f814f68f8
bayuiqballl <bayuiqballl13@gmail.com>
2020-03-23 11:58:43 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layout/main')
+
+@section('title', 'Simple Laravel CRUD')
+
+
+
+@section('container')
+<div class="container">
+  <div class="row">
+    <div class="col-10">
+      <h1 class="mt-3">Hello, world!</h1>
+    </div>
+  </div>
+</div>
+@endsection
